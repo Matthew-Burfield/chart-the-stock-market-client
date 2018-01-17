729f1aba3d221a2d7ad12ee67ea40d66ae1cc9d3
Matthew-Burfield <m1.burfield@gmail.com>
2018-01-18 08:41:58 +1000

    updated prettier and eslint

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => (
+	<main className="container">
+		<div thing={3} anotherThing={'string'} yetAnotherThing={4}>
+			<h1>hello world!</h1>
+			<p>If you see this everything is working!!!</p>
+		</div>
+	</main>
+)
+
+export default App
